e4f54966de49d96fa42211148c7ce7cf731a8e21
feruzbekqahramov <feqahramnov@mail.com>
2023-12-10 16:32:33 +0500

    homework end

---
+++ index.js
@@ -0,0 +1,79 @@
+// 1-masala
+// function topuvchiElementlar(arr) {
+//     let result = [];
+  
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] % 5 === 0 || arr[i] % 3 === 0) {
+//         result.push(arr[i]);
+//       }
+//     }
+  
+//     return result;
+//   }
+//   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30];
+//   console.log(topuvchiElementlar(numbers)); 
+
+// 2-masala
+// function string(arg) {
+// const arg = [false, true, end, NaN, undefined];
+// arr.unshift('^__^');
+// }
+
+// 3-masala
+// function sum(arr) {
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] % 2 == 1) {
+//             sum += arr[i];
+//         }
+//     }
+//     return sum;
+// }
+
+// 4-masala
+// function sum(arr) {
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] % 2 != 0) {
+//             sum.push(arr[i])
+//         }
+//     }
+//     return sum;
+// }
+
+// 5-masala
+// function wordLenght(arg){
+//     const arr = []
+//     for (let i of arg) {
+//         if(i.length > 5){
+//             arr.push(i);
+//         }
+//     }
+//     return arr
+// }
+
+// 6-masala
+// function tub(arg) {
+// let arr1 = [];
+//   for (const iterator of arr) {
+//     if(iterator % 2 == 1) {
+//         arr1.push(iterator)
+//     } 
+//   }
+//   return arr1;
+// };
+
+// 7-masala
+// function topuvchiElementlar(arr) {
+//     let result = [];
+  
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] % 7 === 0 && arr[i] % 3 === 0) {
+//         result.push(arr[i]);
+//       }
+//     }
+  
+//     return result;
+//   }
+//   let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30];
+//   console.log(topuvchiElementlar(numbers)); 
